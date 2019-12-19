7374189fd62c5578a613073c8b4e49f4790088b3
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-19 15:20:29 +0300

    add url to add new project

--- budgetapp/urls.py
+++ budgetapp/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
-    path('',views.project_list,name='list'),
+    path('',views.index,name='index'),
+    path('list',views.project_list,name='list'),
     path('add', views.ProjectCreateView.as_view(),name = 'add'),
     path('<slug:project_slug>',views.project_detail, name = 'detail'),
+    
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
